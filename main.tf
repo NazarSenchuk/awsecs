d24c75780aa6a9ac2b2f89957d6c7447e21cfb6d
NazarSenchuk <senchuknazar6@gmail.com>
2026-01-28 17:44:41 +0200

    Release v1.1.0

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.84.0"
+      source = "hashicorp/aws"
+      version = "6.28.0"
     }
   }
 }
